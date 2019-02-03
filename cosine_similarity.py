dac5088a46fc873b56ffd5a148a15db7c9606ff3
Brooks A Karlik <bkarlik@unr.edu>
2019-02-02 18:53:49 -0800

    aya

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -1,1 +1,11 @@
-import sklearn
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from scipy.spatial.distance import pdist, squareform
+
+
+print('file #1')
+
+if __name__ == 'cosine_similarity':
+    # this code will not run
+
+    print('haha')
---
+++ test.py
@@ -0,0 +1,5 @@
+
+print('file #2 ')
+import cosine_similarity
+print('file #2 again')
+print(__name__)
