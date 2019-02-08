f5db1620412fdd0b2ff6825ba7302c3b54735e84
VanillaBrooks <brookskarlik@gmail.com>
2019-02-07 21:46:05 -0800

    added threaded hashmap base

--- cos_sim_rust/src/main.rs
+++ cos_sim_rust/src/main.rs
@@ -1,4 +1,7 @@
 use hashbrown::HashMap;
+use parking_lot::RwLock;
+use std::sync::Arc;
+use std::thread;
 
 fn cosine_sim(orginal_user_vec: Vec<Vec<u32>>, second_user_vec: Vec<Vec<u32>>) -> f32 {
     let user_2_magnitudes: Vec<f32> = second_user_vec.iter().map(|v| magnitude(v)).collect::<Vec<_>>();
@@ -48,7 +51,8 @@ fn dot_product(vec_1: &Vec<u32>, vec_2: &Vec<u32>) -> f32{
 }
 
 
-fn cmp(original: &String, indexer: &mut HashMap<String, usize>) -> Vec<u32> {
+fn cmp(original: &String, indexer_lock: &Arc<RwLock<HashMap<String, usize>>>) -> Vec<u32> {
+    let indexer = indexer_lock.read();
     // initialize a vector full of zeroes up until the current length of the hashmap
     let mut counts  : Vec<u32> = vec![0; indexer.len()];
 
@@ -58,7 +62,8 @@ fn cmp(original: &String, indexer: &mut HashMap<String, usize>) -> Vec<u32> {
                 counts[*x] += 1;
             },
             None =>{
-                indexer.insert(word.to_string(), indexer.len());
+                let mut index_writer = indexer_lock.write();
+                index_writer.insert(word.to_string(), indexer.len());
                 counts.push(1);
             },
         }
@@ -68,40 +73,59 @@ fn cmp(original: &String, indexer: &mut HashMap<String, usize>) -> Vec<u32> {
 
 }
 
-fn count_occurances<'a>(original: Vec<String>, compare: Vec<String>) -> (Vec<Vec<u32>>, Vec<Vec<u32>>) {
-    let mut indexer : HashMap<String, usize>= HashMap::new();
+fn count_occurances<'a>(original_lock: Arc<RwLock<Vec<String>>>, compare: Vec<String>,  indexer_lock: Arc<RwLock<HashMap<String, usize>>>) -> (Vec<Vec<u32>>, Vec<Vec<u32>>) {
+    let original = original_lock.read();
 
     let mut orig_count : Vec<Vec<u32>> = Vec::new();
-    for phrase in original{
-        orig_count.push(cmp(&phrase, &mut indexer));
+
+    for phrase in original.iter(){
+        orig_count.push(cmp(&phrase, &indexer_lock));
     }
 
     let mut comp_count : Vec<Vec<u32>> = Vec::new();
     for phrase in compare{
-        comp_count.push(cmp(&phrase, &mut indexer));
+        comp_count.push(cmp(&phrase, &indexer_lock));
     }
 
-    for row in &mut orig_count{
-        row.append(&mut vec![0; indexer.len() - row.len()]);
-
-    }
-
-    for  row in &mut comp_count{
-        row.append(&mut vec![0; indexer.len() - row.len()]);
-    }
+//    for row in &mut orig_count{
+ //       row.append(&mut vec![0; indexer.len() - row.len()]);
+//
+//    }
+//
+//    for  row in &mut comp_count{
+//        row.append(&mut vec![0; indexer.len() - row.len()]);
+//~    }
 
     (orig_count, comp_count)
 
 }
 
+fn threading (original_phrases: Vec<String>,  phrases_to_compare: Vec<String>, thread_count: usize)-> (){
+    let map : HashMap<String, usize>= HashMap::new();
+    let indexer = Arc::new(RwLock::new(map));
+    let original_phrases_lock = Arc::new(RwLock::new(original_phrases));
+
+
+    let num_chunks = phrases_to_compare.len() / thread_count;
+
+    for chunk_to_compare in phrases_to_compare.chunks(num_chunks){
+
+        let index_clone = Arc::clone(&indexer);
+        let original_phrases_clone =  Arc::clone(&original_phrases_lock);
+
+        thread::spawn(move || {
+            count_occurances(original_phrases_clone, chunk_to_compare.to_vec(), index_clone)
+        });
+
+    }
+
+
+
+}
+
 fn main() {
     let phrases_1 : Vec<String> = vec!["sample".to_string(), "phrase".to_string()];
-    let phrases_2 : Vec<String> = vec!["sample".to_string(), "here".to_string()];
-    println!{"{:?}", phrases_1}
+    let phrases_2 : Vec<String> = vec!["sample".to_string(), "here".to_string(), "asdads".to_string(), "asdasfhg".to_string(),"sample".to_string(), "here".to_string(), "asdads".to_string(), "asdasfhg".to_string(),"sample".to_string(), "here".to_string(), "asdads".to_string(), "asdasfhg".to_string(),"sample".to_string(), "here".to_string(), "asdads".to_string(), "asdasfhg".to_string(),"sample".to_string(), "here".to_string(), "asdads".to_string(), "asdasfhg".to_string(),"sample".to_string(), "here".to_string(), "asdads".to_string(), "asdasfhg".to_string(),"sample".to_string(), "here".to_string(), "asdads".to_string(), "asdasfhg".to_string() ];
 
-    let (v1, v2) = count_occurances(phrases_1, phrases_2);
-    println!{"v1 {:?} v2 {:?}", v1, v2}
-    let similarities = cosine_sim(v1, v2);
-    println!{"{:?}", similarities }
-
+    threading(phrases_1, phrases_2, 5);
 }
