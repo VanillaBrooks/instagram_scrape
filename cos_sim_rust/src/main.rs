ff92877a0095cbb7559270b3b4278f4982fc180b
VanillaBrooks <brookskarlik@gmail.com>
2019-02-08 02:56:46 -0800

    finished up threading bugfixes - need overflow patch

--- cos_sim_rust/src/main.rs
+++ cos_sim_rust/src/main.rs
@@ -2,6 +2,8 @@ use hashbrown::HashMap;
 use parking_lot::RwLock;
 use std::sync::Arc;
 use std::thread;
+use rayon;
+
 
 fn cosine_sim(orginal_user_vec: Vec<Vec<u32>>, second_user_vec: Vec<Vec<u32>>) -> f32 {
     let user_2_magnitudes: Vec<f32> = second_user_vec.iter().map(|v| magnitude(v)).collect::<Vec<_>>();
@@ -52,21 +54,45 @@ fn dot_product(vec_1: &Vec<u32>, vec_2: &Vec<u32>) -> f32{
 
 
 fn cmp(original: &String, indexer_lock: &Arc<RwLock<HashMap<String, usize>>>) -> Vec<u32> {
-    let indexer = indexer_lock.read();
+    let mut indexer = indexer_lock.read();
     // initialize a vector full of zeroes up until the current length of the hashmap
-    let mut counts  : Vec<u32> = vec![0; indexer.len()];
+    
 
+    let size;
+    {
+        let indexer = indexer_lock.read();
+        size = indexer.len().clone();
+    }
+
+    let mut counts  : Vec<u32> = vec![0; size];
+    let mut need_reader_flag = true;
+
+
+    println!{"after indexer read and counts"}
+
+    let mut write = false;
     for word in original.split(" "){
-        match indexer.get(word){
-            Some(x) => {
-                counts[*x] += 1;
-            },
-            None =>{
-                let mut index_writer = indexer_lock.write();
-                index_writer.insert(word.to_string(), indexer.len());
-                counts.push(1);
-            },
+        println!{"word is {}", word}
+        {
+            let indexer=  indexer_lock.read();
+            println!{"got indexer lock"}
+            match indexer.get(word){
+                Some(x) =>counts[*x] +=1,
+                None => write = true
+            }
+            println!{"after match"}
         }
+
+        if write{
+            println!{"in write"}
+            write = false;
+            let mut indexer_write = indexer_lock.write();
+            println!{"got indexer_write lock"}
+            let length = indexer_write.len().clone();
+            indexer_write.insert(word.to_string(), length);
+        }
+        
+
     }
 
     return counts
@@ -75,12 +101,15 @@ fn cmp(original: &String, indexer_lock: &Arc<RwLock<HashMap<String, usize>>>) ->
 
 fn count_occurances<'a>(original_lock: Arc<RwLock<Vec<String>>>, compare: Vec<String>,  indexer_lock: Arc<RwLock<HashMap<String, usize>>>) -> (Vec<Vec<u32>>, Vec<Vec<u32>>) {
     let original = original_lock.read();
+    println!{"starting a count_occurances function"}
+    
 
     let mut orig_count : Vec<Vec<u32>> = Vec::new();
 
     for phrase in original.iter(){
         orig_count.push(cmp(&phrase, &indexer_lock));
     }
+    println!{"finished original.iter()"}
 
     let mut comp_count : Vec<Vec<u32>> = Vec::new();
     for phrase in compare{
@@ -100,26 +129,27 @@ fn count_occurances<'a>(original_lock: Arc<RwLock<Vec<String>>>, compare: Vec<St
 
 }
 
-fn threading (original_phrases: Vec<String>,  phrases_to_compare: Vec<String>, thread_count: usize)-> (){
+fn threading (original_phrases: Vec<String>,  phrases_to_compare: Vec<String>, thread_count: usize)-> () {
     let map : HashMap<String, usize>= HashMap::new();
     let indexer = Arc::new(RwLock::new(map));
     let original_phrases_lock = Arc::new(RwLock::new(original_phrases));
 
 
     let num_chunks = phrases_to_compare.len() / thread_count;
+    let num_chunks = 15;
 
-    for chunk_to_compare in phrases_to_compare.chunks(num_chunks){
+    rayon::scope( |t| {
+        for chunk_to_compare in phrases_to_compare.chunks(num_chunks){
+            println!{"chunk is {:?}", chunk_to_compare}
+            let index_clone = Arc::clone(&indexer);
+            let original_phrases_clone =  Arc::clone(&original_phrases_lock);
 
-        let index_clone = Arc::clone(&indexer);
-        let original_phrases_clone =  Arc::clone(&original_phrases_lock);
+            t.spawn( move |_| {
+                count_occurances(original_phrases_clone, chunk_to_compare.to_vec(), index_clone);
+            });
 
-        thread::spawn(move || {
-            count_occurances(original_phrases_clone, chunk_to_compare.to_vec(), index_clone)
-        });
-
-    }
-
-
+        }
+    });
 
 }
 
