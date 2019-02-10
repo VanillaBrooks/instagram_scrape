855167fb46bb75310b864d12f43d8852f299a9f1
Brooks A Karlik <bkarlik@unr.edu>
2019-02-09 18:42:17 -0800

    add base functions for constructing .graphml files

---
+++ network_graph.py
@@ -0,0 +1,12 @@
+import networkx
+#import pymysql
+
+def get_sql_data(list_of_users):
+    # construct mysql query to pull all data from
+    pass
+
+def build_graph():
+    # export a graph of all nodes in the swarm
+    pass
+
+def
